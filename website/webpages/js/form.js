648232feec85a50fa67c464518a6b6ac5c180ac5
Manpreet-Singh123 <67155843+Manpreet-Singh123@users.noreply.github.com>
2023-08-04 20:43:21 +0530

    Update form.js

--- website/webpages/js/form.js
+++ website/webpages/js/form.js
@@ -1,13 +1,13 @@
 function sendEmail(){
     Email.send({
-        Host : "smtp.gmail.com",
-        Username : "frmsrciiser@gmail.com",
-        Password : "aman@2223",
+        SecureToken: "edfb0654-1f7c-4184-b18e-0d7419ac0128" ,
         To : 'singhmanpreet10918@gmail.com',
-        From : document.getElementById("email").value,
+        From : "frmsrciiser@gmail.com",
         Subject : "New Contact form Enquiry",
-        Body : "And this is the body"
+        Body : "Name: " + document.getElementById("name").value
+        + "<br> Email: " + document.getElementById("email").value
+        + "<br> Message: " + document.getElementById("message").value
     }).then(
-      message => alert(message)
+      message => alert("Message sent successfuly")
     );
 }
